1ce52574e34a2d6d6b841eda3a85ed56e7241da5
AnastasiyaIlkevich <sirin199@yandex.ru>
2022-12-13 22:49:36 +0300

    new project

---
+++ src/main/java/com/clevertec/generationCashReceipt/GenerationCashReceiptApplication.java
@@ -0,0 +1,13 @@
+package com.clevertec.generationCashReceipt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GenerationCashReceiptApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GenerationCashReceiptApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/clevertec/generationCashReceipt/GenerationCashReceiptApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clevertec.generationCashReceipt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GenerationCashReceiptApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
